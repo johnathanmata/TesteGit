154c6acb9f9a2cca15e82cb2d15f6af8ea58fc8c
Johnathan Mata <johnathancj08@gmail.com>
2019-07-29 17:47:20 -0300

    Projeto Teste

---
+++ Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio {
+    class Employee {
+        private int id;
+        private string name;
+        private double salary;
+
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public double Salary { get => salary; set => salary = value; }
+
+        public void UpdateSalary(double percent) {
+            this.salary *= (1.0 + (percent / 100));
+        }
+
+        public override string ToString() {
+            return Id + "," + Name + ", " + Salary;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercicio {
+    class Program {
+        static void Main(string[] args) {
+            try {
+                List<Employee> list = new List<Employee>();
+                do {
+                    Console.Clear();
+                    Console.WriteLine("1- Increment Employee");
+                    Console.WriteLine("2- Show Emplyees");
+                    Console.WriteLine("3- Alter Salary of Emplyees");
+                    int option = int.Parse(Console.ReadLine());
+
+                    switch (option) {
+                        case 1: //Increment Employee
+                            Console.WriteLine("How many employees will be registred?");
+                            int amount = int.Parse(Console.ReadLine());
+                            for (int i = 0; i < amount; i++) {
+                                Employee a = new Employee();
+                                Console.Write("Id: ");
+                                a.Id = int.Parse(Console.ReadLine());
+                                Console.Write("Name: ");
+                                a.Name = Console.ReadLine();
+                                Console.Write("Salary: ");
+                                a.Salary = double.Parse(Console.ReadLine());
+                                list.Add(a);
+                                Console.WriteLine();
+                            }
+                            break;
+
+                        case 2: //Show Employees
+                            if (list.Count != 0) {
+                                foreach (Object obj in list) {
+                                    Console.WriteLine(obj);
+                                }
+                            } else
+                                Console.WriteLine("Don't have registered Employees.");
+                            Console.ReadLine();
+                            break;
+
+                        case 3: //Update Employee Salary
+                            Console.Clear();
+                            Console.WriteLine("Enter the employee Id that will have salary increase: ");
+                            int upSalarayEmployee = int.Parse(Console.ReadLine());
+                            Employee up = list.Find(x => x.Id == upSalarayEmployee);
+                            if (up != null)
+                                up.UpdateSalary(10);
+                            else {
+                                Console.WriteLine("Employee is not found");
+                            }
+                            break;
+                    }
+                } while (true);
+
+            }
+            catch {
+                Console.WriteLine("Invalid Option");
+            }
+        }
+    }
+}
